24767d1de335f09d04bf392c2b5a07352a212bc1
AlexAndr <o.andrusenko97@outlook.com>
2018-08-08 17:00:56 +0300

    Adds the whole project"

---
+++ GoodStore/GoodStore/Program.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.IO;
+
+namespace GoodStore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StartWork();
+        }
+
+        static void StartWork()
+        {
+            while (true)
+            {
+                try
+                {
+                    ShowMainMenu();
+                    Menu menuChoise = (Menu)int.Parse(Console.ReadLine());
+
+                    using (var repo = new Repository())
+                    {
+                        switch (menuChoise)
+                        {
+                            case Menu.AddGoods:
+                                AddGoods(repo);
+                                break;
+                            case Menu.AddBatch:
+                                AddBatch(repo);
+                                break;
+                            case Menu.ShowGoods:
+                                ShowGoods(repo);
+                                break;
+                            case Menu.Exit:
+                                break;
+                            default:
+                                Console.WriteLine("Wrong menu point. Try again");
+                                break;
+                        }
+                    }
+                    if (menuChoise == Menu.Exit) break;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+
+        static void ShowMainMenu()
+        {
+            Console.WriteLine("What are you want to do?\n Choose:");
+            Console.WriteLine("1. Add goods");
+            Console.WriteLine("2. Add batch");
+            Console.WriteLine("3. Show goods");
+            Console.WriteLine("4. Exit");
+        }
+
+        static void AddGoods(Repository repo)
+        {
+            Goods goods = new Goods();
+
+            Console.WriteLine("Enter all values:");
+            Console.WriteLine("Name:");
+            goods.Name = Console.ReadLine();
+
+            Console.WriteLine("Unit:");
+            goods.Unit = Console.ReadLine();
+
+            Console.WriteLine("UnitPrice:");
+            goods.UnitPrice = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Quantity:");
+            goods.Quantity = int.Parse(Console.ReadLine());
+
+            repo.AddGoods(goods);
+        }
+
+        static void AddBatch(Repository repo)
+        {
+            var batch = new Batch();
+
+            Console.WriteLine("Enter all values:");
+            Console.WriteLine("Operation type:");
+            batch.OperationType = Console.ReadLine();
+
+            Console.WriteLine("Date:");
+            batch.Date = DateTime.Parse(Console.ReadLine());
+
+            var batchContent = new BatchContent();
+            Console.WriteLine("GoodsId:");
+            batchContent.GoodsId = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("BatchId:");
+            batchContent.BatchId = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Quanity:");
+            batchContent.Quantity = int.Parse(Console.ReadLine());
+
+            repo.AddBatch(batch, batchContent);
+        }
+
+        static void ShowGoods(Repository repo)
+        {
+            var goods = repo.GetGoods();
+
+            if (goods.Count < 1)
+            {
+                Console.WriteLine("There are no goods");
+                return;
+            }
+
+            Console.WriteLine($"GoodsId\tName\tUnit\tUnitPrice\tQuantity");
+            Console.WriteLine(new string('-', 50));
+
+            foreach (var g in goods)
+                Console.WriteLine($"{g.GoodsId} \t {g.Name} \t {g.Unit} \t {g.UnitPrice} \t {g.Quantity}");
+
+            Console.WriteLine(new string('-', 50));
+        }
+
+        enum Menu
+        {
+            AddGoods = 1, AddBatch, ShowGoods, Exit
+        }
+    }
+}
---
+++ GoodStore/GoodStore/Repository.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Text;
+
+namespace GoodStore
+{
+    class Repository : IDisposable
+    {
+        readonly string conStr = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=" + Directory.GetCurrentDirectory() +
+            "\\AppData\\GoodStore.mdf;Integrated Security=True;Connect Timeout=30";
+
+        SqlConnection _con;
+
+        public Repository()
+        {
+            _con = new SqlConnection(conStr);
+        }
+
+        public void Dispose()
+        {
+            _con.Dispose();
+        }
+
+        public void AddGoods(Goods goods)
+        {
+            try
+            {
+                string commandString = @"INSERT INTO Goods (Name, Unit, UnitPrice, Quantity) 
+                                    VALUES(@Name, @Unit, @UnitPrice, @Quantity)";
+
+                var command = new SqlCommand(commandString, _con);
+                command.Parameters.AddWithValue("@Name", goods.Name);
+                command.Parameters.AddWithValue("@Unit", goods.Unit);
+                command.Parameters.AddWithValue("@UnitPrice", goods.UnitPrice);
+                command.Parameters.AddWithValue("@Quantity", goods.Quantity);
+                _con.Open();
+                command.ExecuteNonQuery();
+                _con.Close();
+            }
+            catch (Exception)
+            {
+                _con.Close();
+                throw;
+            }
+        }
+
+        public List<Goods> GetGoods()
+        {
+            var goods = new List<Goods>();
+
+            try
+            {
+                string commandString = "SELECT * FROM Goods";
+
+                SqlCommand command = new SqlCommand(commandString, _con);
+                _con.Open();
+
+                using (SqlDataReader reader = command.ExecuteReader(CommandBehavior.CloseConnection))
+                {
+                    while (reader.Read())
+                    {
+                        goods.Add(new Goods
+                        {
+                            GoodsId = (int)reader["GoodsId"],
+                            Name = (string)reader["Name"],
+                            Unit = (string)reader["Unit"],
+                            UnitPrice = (double)reader["UnitPrice"],
+                            Quantity = (int)reader["Quantity"]
+                        });
+                    }
+                }
+                _con.Close();
+            }
+            catch (Exception)
+            {
+                _con.Close();
+                throw;
+            }
+
+            return goods;
+        }
+
+        public void AddBatch(Batch batch, BatchContent batchContent)
+        {
+            try
+            {
+                string batchCommandString = @"INSERT INTO Batch (OperationType, Date)
+                                            VALUES (@OperationType, @Date)";
+
+                var batchCommand = new SqlCommand(batchCommandString, _con);
+                batchCommand.Parameters.AddWithValue("@OperationType", batch.OperationType);
+                batchCommand.Parameters.AddWithValue("@Date", batch.Date);
+                _con.Open();
+                batchCommand.ExecuteNonQuery();
+
+                string batchContentCommandString =
+                    @"INSERT INTO BatchContent (GoodsId, BatchId, Quantity) VALUES (@GoodsId, @BatchId, @Quantity)";
+
+                var batchContentCommand = new SqlCommand(batchContentCommandString, _con);
+                batchContentCommand.Parameters.AddWithValue("@GoodsId", batchContent.GoodsId);
+                batchContentCommand.Parameters.AddWithValue("@BatchId", batchContent.BatchId);
+                batchContentCommand.Parameters.AddWithValue("@Quantity", batchContent.Quantity);
+                batchContentCommand.ExecuteNonQuery();
+
+                _con.Close();
+            }
+            catch (Exception)
+            {
+                _con.Close();
+                throw;
+            }
+        }
+    }
+
+    class Goods
+    {
+        public int GoodsId { get; set; }
+        public string Name { get; set; }
+        public string Unit { get; set; }
+        public double UnitPrice { get; set; }
+        public int Quantity { get; set; }
+    }
+
+    class Batch
+    {
+        public int BatchId { get; set; }
+        public string OperationType { get; set; }
+        public DateTime Date { get; set; }
+    }
+
+    class BatchContent
+    {
+        public int GoodsId { get; set; }
+        public int BatchId { get; set; }
+        public int Quantity { get; set; }
+    }
+}
